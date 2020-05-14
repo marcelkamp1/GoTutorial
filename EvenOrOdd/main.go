c48b1c47b9b7c5ab2e1c49a64031792f984c0524
Marcel Kamp <marcel.kamp@telekom.de>
2020-05-14 12:47:26 +0200

    added underscore for unused index

--- EvenOrOdd/main.go
+++ EvenOrOdd/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	ints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i := range ints {
+	for _, i := range ints {
 		if ints[i]%2 != 0 {
 			fmt.Printf("%v is odd \n", ints[i])
 		}
