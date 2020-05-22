1391b4231a42b29ed8fa54006128b015b1b048bf
marcelkamp1 <marcelkamp1@gmx.de>
2020-05-22 03:43:00 +0200

    solved CountDivisors, created #2 Clock

---
+++ Codewars/#2 Clock - Kyu8/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(past(0, 1, 1))
+}
+
+func past(h, m, s int) int {
+	// your code here
+	msMinute := 60000
+	msSecond := msMinute / 60
+	msHour := msMinute * 60
+
+	return (msHour * h) + (msMinute * m) + (msSecond * s)
+}
+
+/*
+Clock shows 'h' hours, 'm' minutes and 's' seconds after midnight.
+
+Your task is to make 'Past' function which returns time converted to milliseconds.
+
+Example:
+past(0, 1, 1) == 61000
+Input constraints: 0 <= h <= 23, 0 <= m <= 59, 0 <= s <= 59
+*/
